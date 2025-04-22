c93c82d9c699e0e7d5126bae92836c390100eec8
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2025-04-22 23:35:22 +0700

    ...

--- src/api/settings.py
+++ src/api/settings.py
@@ -3,7 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_socketio import SocketIO
 
-SocketIO(cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(cors_allowed_origins="*", async_mode='eventlet')
 db = SQLAlchemy()
 jwt = JWTManager()
 bcrypt = Bcrypt()
