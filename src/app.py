03b9c0369b0f18cb292a77ec61501afd952128f9
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2025-04-24 10:07:59 +0700

    update to debug

--- src/app.py
+++ src/app.py
@@ -331,9 +331,13 @@ async def tutorial(subpath=None):
 def review():
     return render_template("index.html", name="review.html")
 
+@app.route("/contact", methods=["GET"])
+def contact():
+    return render_template("index.html", name="team.html")
+
 
 
 # Thay đổi dòng này ở cuối file
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Railway sẽ cung cấp PORT qua biến môi trường
-    socketio.run(app, host="0.0.0.0", port=port)
+    app.run(port=port, host="0.0.0.0", debug=True)  # Chạy ứng dụng Flask trên tất cả các địa chỉ IP của máy chủ
