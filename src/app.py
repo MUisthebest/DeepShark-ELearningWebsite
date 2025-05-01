5bc111d70e5d0b7c14291751c47ce1368331ae7b
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2025-05-01 20:33:46 +0700

    check

--- src/app.py
+++ src/app.py
@@ -313,4 +313,4 @@ def contact():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  
-    app.run(port=port, host="0.0.0.0", debug=True) 
+    socketio.run(app, host="0.0.0.0", port=port)
