fc80d13c6f0ba7277670ae18e466b8c9153b9193
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2025-05-02 12:38:20 +0700

    Revert "damage!!!!"
    This reverts commit 1dff6011dbdab7cbc7616ea0bc9ca674fca0f145.

--- src/api/ai_models/calculate_embedding.py
+++ src/api/ai_models/calculate_embedding.py
@@ -2,19 +2,16 @@ from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import os
 
-finetuned_modelSBERT = SentenceTransformer("BanhMiKepThit015/Deepshark-Paraphrase-MiniLM-v2")
-finetuned_modelSBERT.save("local_model_path")
+finetuned_modelSBERT = None
 
 def load_model():
-    model_path = "local_model_path"
-    if os.path.exists(model_path):
-        return SentenceTransformer(model_path)
-    else:
-        model = SentenceTransformer("BanhMiKepThit015/Deepshark-Paraphrase-MiniLM-v2")
-        model.save(model_path)
-        return model
+    global finetuned_modelSBERT 
+    if finetuned_modelSBERT is None:
+        finetuned_modelSBERT  = SentenceTransformer("BanhMiKepThit015/Deepshark-Paraphrase-MiniLM-v2")
+    return finetuned_modelSBERT
 
 def get_query_embedding(query):
-    model = load_model()
-    return model.encode(query)
+    finetuned_modelSBERT = load_model()
+    query_embedding = finetuned_modelSBERT.encode(query)
+    return query_embedding
 
