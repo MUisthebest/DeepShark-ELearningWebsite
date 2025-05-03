e184de05a72fb80d584f7c28c322d7642876cfc7
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2025-05-04 01:11:52 +0700

    ................

--- src/api/ai_models/calculate_embedding.py
+++ src/api/ai_models/calculate_embedding.py
@@ -1,15 +1,23 @@
 from sentence_transformers import SentenceTransformer
-import docker
+import os
 
 class ModelLoader:
     def __init__(self):
         self.model_name = "BanhMiKepThit015/Deepshark-Paraphrase-MiniLM-v2"
+        self.local_dir = "model"
         self.model = None
 
     def load(self):
         if self.model is None:
-            self.model = SentenceTransformer(self.model_name)
-            self.model.save("model")
+            if os.path.exists(self.local_dir):
+                # Load from local
+                print("Loading model from local directory...")
+                self.model = SentenceTransformer(self.local_dir)
+            else:
+                # Load online and save
+                print("Downloading model from HuggingFace...")
+                self.model = SentenceTransformer(self.model_name)
+                self.model.save(self.local_dir)
         return self.model
 
 def get_query_embedding(query):
