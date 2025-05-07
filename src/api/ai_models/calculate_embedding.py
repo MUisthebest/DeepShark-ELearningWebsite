dc8e96bb7d0e78b79939e08b308a574dd5d7990d
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2025-05-08 06:52:57 +0700

    fix tiny

--- src/api/ai_models/calculate_embedding.py
+++ src/api/ai_models/calculate_embedding.py
@@ -4,20 +4,12 @@ import os
 class ModelLoader:
     def __init__(self):
         self.model_name = "BanhMiKepThit015/Deepshark-Paraphrase-MiniLM-v2"
-        self.local_dir = "model"
         self.model = None
 
     def load(self):
         if self.model is None:
-            if os.path.exists(self.local_dir):
-                # Load from local
-                print("Loading model from local directory...")
-                self.model = SentenceTransformer(self.local_dir)
-            else:
-                # Load online and save
-                print("Downloading model from HuggingFace...")
-                self.model = SentenceTransformer(self.model_name)
-                self.model.save(self.local_dir)
+            print("Loading model from HuggingFace Hub...")
+            self.model = SentenceTransformer(self.model_name)
         return self.model
 
 def get_query_embedding(query):
